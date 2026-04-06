dc8c8bcb8f4156a8cca0c7cc7f8e9eb401f5f83f
minokingdom <ektjtthsrk@gmail.com>
2026-04-07 00:04:43 +0900

    Update GS URL to latest and best version

--- main.js
+++ main.js
@@ -575,7 +575,7 @@ function animateCount(el, start, end, duration) {
 }
 
 // ── 구글 스프레드시트 연동 설정 ──
-const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbz6tgPzLQAqKr5z5RGmT5rvO0vwVuugGKMEgOJysAR45GwesN7YvntyTNLeBfZEoHT-UQ/exec";
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzPk_Meh8DkYJ2BPxQ9mX6qOGWei31fUgS0XubNHw1LB0l2-MFVoKxk2zeiW9C-22_hWw/exec";
 
 function initSupportForm() {
   const form = document.getElementById('support-form');
